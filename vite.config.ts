7673efe3ac12fba6b1aa4f50d3f18a70c42ace20
Hamzar <60004028+xhamzar@users.noreply.github.com>
2025-11-20 14:19:11 +0700

    Configure base path and output directory in Vite config
    Set output directory for build and adjust base path for assets.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // PENTING: Memastikan asset dimuat dengan benar di hosting statis
   build: {
     chunkSizeWarningLimit: 1000,
+    outDir: 'dist',
   }
 })
